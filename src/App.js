3fba4809c5676651bcf7e4c2e1c488ea3450b3c7
yujin <fgh9210@gmail.com>
2020-08-27 21:04:57 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Potato from "./Potato";
+
+function App() {
+  return (
+    <div className="App"> hello!!
+    <Potato />
+
+    </div>
+
+  );
+}
+
+export default App;
