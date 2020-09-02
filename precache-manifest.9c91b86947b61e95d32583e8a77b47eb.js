b67b2f84870bcafe9388eb687dea02f96113f283
yujin <fgh9210@gmail.com>
2020-09-02 21:44:44 +0900

    Updates

---
+++ precache-manifest.9c91b86947b61e95d32583e8a77b47eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea79539e3ac4dc398f0a20924a812b1",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "d7a842426847332ce3f8",
+    "url": "/movie_app_2020/static/css/main.e8891b0e.chunk.css"
+  },
+  {
+    "revision": "a7d22b2c00d8d326c863",
+    "url": "/movie_app_2020/static/js/2.300fc6d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.300fc6d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a842426847332ce3f8",
+    "url": "/movie_app_2020/static/js/main.a407147b.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
